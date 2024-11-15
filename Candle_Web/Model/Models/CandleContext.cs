0325277547ae90f4d7dc7ba1d16ca70a020d8668
NguyenNha <nguyenngothanhnha2003kg@gmail.com>
2024-11-15 13:57:34 +0700

    add CICD

--- Candle_Web/Model/Models/CandleContext.cs
+++ Candle_Web/Model/Models/CandleContext.cs
@@ -30,24 +30,24 @@ namespace Model.Models
         public virtual DbSet<User> Users { get; set; } = null!;
         public virtual DbSet<Zalopay> Zalopays { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer(GetConnectionString());
-            }
-        }
-        private string GetConnectionString()
-        {
-            IConfiguration config = new ConfigurationBuilder()
-             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", true, true)
-            .Build();
-            var strConn = config["ConnectionStrings:Candle"];
+        //         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //         {
+        //             if (!optionsBuilder.IsConfigured)
+        //             {
+        // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        //                 optionsBuilder.UseSqlServer(GetConnectionString());
+        //             }
+        //         }
+        //         private string GetConnectionString()
+        //         {
+        //             IConfiguration config = new ConfigurationBuilder()
+        //              .SetBasePath(Directory.GetCurrentDirectory())
+        //             .AddJsonFile("appsettings.json", true, true)
+        //             .Build();
+        //             var strConn = config["ConnectionStrings:Candle"];
 
-            return strConn;
-        }
+        //             return strConn;
+        //         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
