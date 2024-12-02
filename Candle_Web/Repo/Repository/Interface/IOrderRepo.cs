acdab72de50ca16510479b7193ff47d5f05f012d
aotungchao0201 <134262535+aotungchao0201@users.noreply.github.com>
2024-12-03 01:55:32 +0700

    Update IOrderRepo.cs

--- Candle_Web/Repo/Repository/Interface/IOrderRepo.cs
+++ Candle_Web/Repo/Repository/Interface/IOrderRepo.cs
@@ -1,23 +1,72 @@
-﻿using Model.Models;
+using Microsoft.EntityFrameworkCore;
+using Model.Models;
+using Repo.Repository.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repo.Repository.Interface
+namespace Repo.Repository
 {
-    public interface IOrderRepo
+    public class OrderRepo : IOrderRepo
     {
-        public Task<List<Order>> GetALL();
-        public Task<Order> Create(Order order);
-        public Task<Order> Update(Order order);
-        public Task<bool> Delete(OrderItem order);
+        private readonly candleContext _context;
 
-        public Task<OrderItem> GetOrderItemById(int id);
-        public Task<Order> GetByOrderId(int id);
+        public OrderRepo(candleContext context)
+        {
+            _context = context;
+        }
+        public async Task<Order> Create(Order data)
+        {
+            _context.Add(data);
+            await _context.SaveChangesAsync();
+            return data;
+        }
 
-        public Task<Order> GetByName(string? order);
+        public async Task<bool> Delete(OrderItem data)
+        {
+            _context.Remove(data);
+            await _context.SaveChangesAsync();
+            return true;
+        }
 
+        public async Task<List<Order>> GetALL()
+        {
+            var data = await _context.Orders.Include(o=>o.User).ToListAsync();
+            return data;
+        }
+
+        public async Task<OrderItem> GetOrderItemById(int id)
+        {
+            var data = await _context.OrderItems.SingleOrDefaultAsync(x => x.OrderId.Equals(id));
+            return data;
+        }
+        public async Task<Order> GetByOrderId(int id)
+        {
+            var data = await _context.Orders.Include(o => o.OrderItems)
+                .ThenInclude(o=>o.Candle)
+                .Include(o=>o.User)
+                .SingleOrDefaultAsync(x => x.OrderId.Equals(id));
+            return data;
+        }
+        public async Task<Order> GetByName(string? name)
+        {
+            var data = await _context.Orders.SingleOrDefaultAsync(x => x.User.Username.Equals(name));
+            return data;
+        }
+
+        public async Task<Order> Update(Order data)
+        {
+            _context.Update(data);
+            await _context.SaveChangesAsync();
+            return data;
+        }
+
+        public async Task<List<Order>> GetByUserId(int id)
+        {
+            var data = await _context.Orders.Include(o=>o.User).Where(x => x.UserId.Equals(id)).ToListAsync();
+            return data;
+        }
     }
 }
