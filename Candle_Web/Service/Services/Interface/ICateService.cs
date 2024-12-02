ea84646092604dd85fe243b78081e6772a37baa4
aotungchao0201 <134262535+aotungchao0201@users.noreply.github.com>
2024-12-03 01:42:06 +0700

    Add files via upload

---
+++ Candle_Web/Service/Services/Interface/ICateService.cs
@@ -0,0 +1,19 @@
+﻿using Model.Models;
+using Service.Modals.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services.Interface
+{
+    public interface ICateService
+    {
+        public Task<List<Category>> GetALL();
+        public Task<CateRequest> Create(CateRequest order);
+        public Task<bool> Delete(int order);
+
+        public Task<List<Candle>> GetCandleByCategoryId(int id);
+    }
+}
