5792fc2ccb9f58bdcfbf05d580198575921c45f9
aotungchao0201 <134262535+aotungchao0201@users.noreply.github.com>
2024-12-03 01:57:36 +0700

    Add files via upload

---
+++ Candle_Web/Repo/Repository/Interface/ICateRepo.cs
@@ -0,0 +1,21 @@
+﻿using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo.Repository.Interface
+{
+    public interface ICateRepo
+    {
+        public Task<List<Category>> GetALL();
+        public Task<Category> Create(Category order);
+        public Task<bool> Delete(Category order);
+
+        public Task<List<Candle>> GetCandleByCategoryId(int id);
+
+        public Task<Category> GetCateById(int id);
+
+    }
+}
