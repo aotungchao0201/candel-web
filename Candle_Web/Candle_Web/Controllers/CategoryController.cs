d25b964fc532638808b3735e3f0db30cf9bc6a95
aotungchao0201 <134262535+aotungchao0201@users.noreply.github.com>
2024-12-02 23:46:32 +0700

    Add files via upload

---
+++ Candle_Web/Candle_Web/Controllers/CategoryController.cs
@@ -0,0 +1,98 @@
+﻿using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Modals.Request;
+using System.Net;
+using Service.Services.Interface;
+
+namespace Candle_Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICateService _cate;
+
+        public CategoryController(ICateService cate)
+        {
+            _cate = cate;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateCate([FromForm] CateRequest candle)
+        {
+            try
+            {
+               
+                var data = await _cate.Create(candle);
+                if (data == null)
+                {
+                    return BadRequest();
+                }
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest();
+            }
+
+        }
+
+        [HttpDelete("delete-cate/{id}")]
+        public async Task<IActionResult> DeleteCandleById(int id)
+        {
+            try
+            {
+                var result = await _cate.Delete(id);
+                if (result)
+                {
+                    return Ok();
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest();
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCandle()
+        {
+            try
+            {
+                var result = await _cate.GetALL();
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpGet("get-by-id/{id}")]
+        public async Task<IActionResult> GetAllCandle(int id)
+        {
+            try
+            {
+                var result = await _cate.GetCandleByCategoryId(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
