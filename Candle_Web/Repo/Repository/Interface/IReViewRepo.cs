fb2fcdaef2f3172582efa4b975a50a2cc662e380
aotungchao0201 <134262535+aotungchao0201@users.noreply.github.com>
2024-12-03 01:56:47 +0700

    Update IReViewRepo.cs

--- Candle_Web/Repo/Repository/Interface/IReViewRepo.cs
+++ Candle_Web/Repo/Repository/Interface/IReViewRepo.cs
@@ -1,58 +1,20 @@
-using Microsoft.EntityFrameworkCore;
 using Model.Models;
-using Repo.Repository.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repo.Repository
+namespace Repo.Repository.Interface
 {
-    public class ReviewRepo : IReViewRepo
+    public interface IReViewRepo
     {
-        private readonly candleContext _context;
+        public Task<List<Review>> GetAllReview();
+        public Task<Review> CreateReview(Review data);
+        public Task<Review> UpdateReview(Review data);
+        public Task<bool> DeleteReview(Review data);
 
-        public ReviewRepo(candleContext context)
-        {
-            _context = context;
-        }
-        public async Task<Review> CreateReview(Review data)
-        {
-            _context.Add(data);
-            await _context.SaveChangesAsync();
-            return data;
-        }
-
-        public async Task<bool> DeleteReview(Review data)
-        {
-            _context.Remove(data);
-            await _context.SaveChangesAsync();
-            return true;
-        }
-
-        public async Task<List<Review>> GetAllReview()
-        {
-            var data = await _context.Reviews.ToListAsync();
-            return data;
-        }
-
-        public async Task<Review> GetReviewById(int id)
-        {
-            var data = await _context.Reviews.SingleOrDefaultAsync(x =>x.ReviewId.Equals(id));
-            return data;
-        }
-
-        public Task<Review> GetReviewByName(string? data)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<Review> UpdateReview(Review data)
-        {
-            _context.Update(data);
-            await _context.SaveChangesAsync();
-            return data;
-        }
+        public Task<Review> GetReviewById(int id);
+        public Task<Review> GetReviewByName(string? data);
     }
 }
