1657f4354bf1e527b52da720ed84f0226930199b
aotungchao0201 <134262535+aotungchao0201@users.noreply.github.com>
2024-12-03 01:56:18 +0700

    Update ICandleRepo.cs

--- Candle_Web/Repo/Repository/Interface/ICandleRepo.cs
+++ Candle_Web/Repo/Repository/Interface/ICandleRepo.cs
@@ -1,69 +1,22 @@
-using Microsoft.EntityFrameworkCore;
 using Model.Models;
-using Repo.Repository.Interface;
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repo.Repository
+namespace Repo.Repository.Interface
 {
-    public class CandleRepo : ICandleRepo
+    public interface ICandleRepo
     {
-        private readonly candleContext _context;
+        public Task<List<Candle>> GetAllCandle();
+        public Task<Candle> CreateCandle(Candle candle);
+        public Task<Candle> UpdateCandle(Candle candle);
+        public Task<bool> DeleteCandle(Candle candle);
 
-        public CandleRepo(candleContext context)
-        {
-            _context = context;
-        }
+        public Task<Candle> GetCandleById(int id);
+        public Task<List<Candle>> GetByCategoryId(int id);
 
-        public async Task<Candle> CreateCandle(Candle candle)
-        {
-            _context.Add(candle);
-            await _context.SaveChangesAsync();
-            return candle;
-        }
-
-        public async Task<bool> DeleteCandle(Candle candle)
-        {
-            _context.Remove(candle);
-            await _context.SaveChangesAsync();
-            return true;
-        }
-
-        public async Task<List<Candle>> GetAllCandle()
-        {
-            var data = await _context.Candles.
-                Include(o=>o.Category).ToListAsync();
-            
-            return data;
-        }
-
-        public async Task<List<Candle>> GetByCategoryId(int id)
-        {
-            var data = await _context.Candles.Include(o => o.Category).Where(x => x.CategoryId.Equals(id)).ToListAsync();
-            return data;
-        }
-
-        public async Task<Candle> GetCandleById(int id)
-        {
-            var data = await _context.Candles.Include(o => o.Category).SingleOrDefaultAsync(x=> x.CandleId.Equals(id));
-            return data;
-        }
-
-        public async Task<Candle> GetCandleByName(string? candle)
-        {
-            var data = await _context.Candles.SingleOrDefaultAsync(x => x.Name.Equals(candle));
-            return data;
-        }
-
-        public async Task<Candle> UpdateCandle(Candle candle)
-        {
-            _context.Update(candle);
-            await _context.SaveChangesAsync();
-            return candle;
-        }
+        public Task<Candle> GetCandleByName(string? candle);
     }
 }
