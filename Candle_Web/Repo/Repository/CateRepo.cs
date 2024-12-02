cc6ab267f739052970fb9b1ff2b4b95e3e201e03
aotungchao0201 <134262535+aotungchao0201@users.noreply.github.com>
2024-12-03 01:35:23 +0700

    Add files via upload

---
+++ Candle_Web/Repo/Repository/CateRepo.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Models;
+using Repo.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo.Repository
+{
+    public class CateRepo : ICateRepo
+    {
+        private readonly candleContext _context;
+
+        public CateRepo(candleContext context)
+        {
+            _context = context;
+        }
+        public async Task<Category> Create(Category data)
+        {
+            _context.Add(data);
+            await _context.SaveChangesAsync();
+            return data;
+        }
+
+        public async Task<bool> Delete(Category data)
+        {
+            _context.Remove(data);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<List<Category>> GetALL()
+        {
+            var data = await _context.Categories.ToListAsync();
+            return data;
+        }
+
+        public async Task<List<Candle>> GetCandleByCategoryId(int id)
+        {
+            var candles = await _context.Candles
+                           .Where(c => c.CategoryId == id)
+                           .ToListAsync();
+
+            return candles;
+        }
+
+        
+
+        public async Task<Category> GetCateById(int id)
+        {
+            var data = await _context.Categories.SingleOrDefaultAsync(x => x.CategoryId.Equals(id));
+            return data;
+        }
+    }
+}
